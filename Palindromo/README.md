a07a77b3944245da5f6ff577cec95c94baf381fc
gaetanovenuto <gaetanovenuto99@live.it>
2024-07-19 16:03:08 +0200

    Esercizio palindromo completato

---
+++ Palindromo/README.md
@@ -0,0 +1,9 @@
+Palidroma
+Chiedere all'utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+
+1. Chiedo all'utente un valore 'parola' e lo salvo.
+
+2. Divido la parola in caratteri tramite "split" e li inserisco in un'array.
+
+3. Creo un ciclo che legge 
---
+++ Palindromo/js/scripts.js
@@ -0,0 +1,40 @@
+// Chiedo all'utente il valore "parola" e lo salvo.
+
+const userWord = prompt('Inserisci una parola');
+console.log (userWord);
+
+// Inizializzo una variabile per utilizzarla all'interno della funzione.
+
+let reverse = 0;
+
+// Creo la funzione da applicare
+
+function reverseString () {
+
+    //  La variabile reverse utilizza il valore fornito dall'utente:
+    //  1. Divide la stringa in un'array con i singoli caratteri;
+    //  2. Inverte l'ordine dei valori all'interno dell'array dall'ultimo al primo;
+    //  3. Riunisce i valori all'interno dell'array in una singola stringa;
+
+    reverse = userWord.split('').reverse().join('');
+
+    // Mi restituisce il valore fornito dall'utente al contrario.
+
+    return reverse;
+}
+
+// Inizializzo una variabile utilizzando la funzione per invertire qualsiasi valore inserito dall'utente.
+
+const reverseUserWord = reverseString(userWord);
+console.log (reverseUserWord);
+
+// Se la parola inserita dall'utente è uguale alla stessa parola invertita al contrario, restituisce l'alert "palindromo"...
+
+if (userWord === reverseUserWord) {
+    alert("La parola " + userWord + " è palindroma!");
+}
+
+// ...altrimenti, restituisce l'alert "NON palindromo".
+else {
+    alert("La parola: " +  userWord + " NON è palindroma!")
+}
